0f6a935a5747620d0bc140e1c6e8255bc18839cf
titusgriebel <titus.griebel@gmx.de>
2024-11-15 09:45:22 +0100

    deleted surplus

--- finetuning/specialists/training/histopathology/throwaway.py
+++
@@ -1,7 +0,0 @@
-from torch_em.data import datasets
-import micro_sam.training as sam_training
-import torch 
-
-checkpoint = torch.load('/scratch-grete/projects/nim00007/sam/models/vanilla/sam_vit_b_01ec64.pth')
-
-print(checkpoint.keys())
